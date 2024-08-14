7594de7a6a95ad37fbd16f1b8600db6a3539aa2b
begzod_eshmatov <eshmatovbegzod16@gmail.com>
2024-08-14 14:54:35 +0500

    .

--- routes/web.php
+++ routes/web.php
@@ -9,17 +9,6 @@ use App\Kurs;
 use App\Maktab;
 use App\Yangilik;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
         
     $olish = Elon::all();
